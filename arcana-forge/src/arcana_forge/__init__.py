3109ee8759855237076560d1b285b88036cb127d
Sunlake <register@milkcat.org>
2026-08-28 09:37:22 +0800

    feat: expose ArcanaForge generation provider API

--- arcana-forge/src/arcana_forge/__init__.py
+++ arcana-forge/src/arcana_forge/__init__.py
@@ -1,12 +1,17 @@
 from .forge import ForgeRegistry, forge
+from .generation import GeneratedAsset, GenerationProvider, PromptFileProvider, generate_collection
 from .schema import CollectionSpec, StyleSpec, SubjectSpec, SymbolicCollection, SymbolicUnit
 
 __all__ = [
     "CollectionSpec",
     "ForgeRegistry",
+    "GeneratedAsset",
+    "GenerationProvider",
+    "PromptFileProvider",
     "StyleSpec",
     "SubjectSpec",
     "SymbolicCollection",
     "SymbolicUnit",
     "forge",
+    "generate_collection",
 ]
