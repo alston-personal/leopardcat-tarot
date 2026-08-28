3aa2b3303c51526207d0d597808ed068bb11873a
Sunlake <register@milkcat.org>
2026-08-28 09:42:50 +0800

    feat: export built-in and JSON symbolic systems

--- arcana-forge/src/arcana_forge/systems/__init__.py
+++ arcana-forge/src/arcana_forge/systems/__init__.py
@@ -1,1 +1,8 @@
-"""Built-in symbolic systems."""
+"""Built-in and data-driven symbolic systems."""
+
+from .base import SymbolicSystem
+from .iching import IChingSystem
+from .json_system import JsonSymbolicSystem
+from .tarot import TarotSystem
+
+__all__ = ["IChingSystem", "JsonSymbolicSystem", "SymbolicSystem", "TarotSystem"]
